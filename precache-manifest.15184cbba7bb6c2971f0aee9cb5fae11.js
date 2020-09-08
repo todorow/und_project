e85c7b81f25d20e58e6e937df36c8aafd007beb9
Viktor Todev <viktortodev@gmail.com>
2020-09-08 23:24:36 +0200

    commit

---
+++ precache-manifest.15184cbba7bb6c2971f0aee9cb5fae11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb57040ab4bed62f737591578ea8dcbd",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "1e7aa15b18830f56bd5f",
+    "url": "@yield('content-viewer-url')/static/css/2.45195411.chunk.css"
+  },
+  {
+    "revision": "80de6f6ef073ff75b46c",
+    "url": "@yield('content-viewer-url')/static/css/main.1757e009.chunk.css"
+  },
+  {
+    "revision": "1e7aa15b18830f56bd5f",
+    "url": "@yield('content-viewer-url')/static/js/2.c0be0b45.chunk.js"
+  },
+  {
+    "revision": "f79bc39b665f8810feb218e2c6043c0e",
+    "url": "@yield('content-viewer-url')/static/js/2.c0be0b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80de6f6ef073ff75b46c",
+    "url": "@yield('content-viewer-url')/static/js/main.221fe509.chunk.js"
+  },
+  {
+    "revision": "05c91422a8ff984def11",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.779b656d.js"
+  }
+]);
